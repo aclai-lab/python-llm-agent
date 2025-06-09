ac7e885c49e3faf89a3a205c3b7df3f498342a3f
Belluxx <78388441+Belluxx@users.noreply.github.com>
2025-06-09 19:14:01 +0200

    Add standard completion exercise

---
+++ complete.py
@@ -0,0 +1,29 @@
+from libs.input_manager import InputManager
+from libs.agent import Agent
+
+
+def main():
+    # Gestisce la terminazione del processo.
+    InputManager.handle_interrupt()
+
+    # Setta il nome dell'utente
+    InputManager.set_name("luke")
+
+    # Crea un'istanza dell'agente
+    agent = Agent(name="Qwen3-4B-Q4_K_M", n_generate=32)
+
+    # Tokenizza una frase
+    token = agent.tokenize("Ciao a tutti")
+    print(token)
+    
+    # De-tokenizza una frase
+    testo = agent.detokenize(token)
+    print(testo)
+    
+    # Completa una frase
+    frase = "Ciao a tutti"
+    agent.complete_text(frase)
+
+
+if __name__ == "__main__":
+    main()
